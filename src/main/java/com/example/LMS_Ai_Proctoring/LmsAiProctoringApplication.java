bd6c34cd98f2da2281d1308e1f3532317d895c72
MannegudemChandana <chandana.m@clientservertech.com>
2026-07-07 11:53:27 +0530

    Initial commit

---
+++ src/main/java/com/example/LMS_Ai_Proctoring/LmsAiProctoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.LMS_Ai_Proctoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsAiProctoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsAiProctoringApplication.class, args);
+	}
+
+}
